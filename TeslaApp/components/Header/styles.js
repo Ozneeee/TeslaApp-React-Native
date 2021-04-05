b7c46d290c40f7272b104935b84dd9accf3deedf
Ozne <57328206+Ozneeee@users.noreply.github.com>
2021-04-05 23:43:57 +0200

    Add files via upload

---
+++ TeslaApp/components/Header/styles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 50,
+        zIndex: 100,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 20,
+    },
+    logo: {
+        width: 100,
+        height: 20,
+        resizeMode: 'contain'
+    },
+    menu: {
+        width: 25,
+        height: 25,
+    }
+});
+
+export default styles;
